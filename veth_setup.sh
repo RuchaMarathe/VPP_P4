ab8bcb0bd203227c697d8a57435561ff4ea7ef6d
Rucha Marathe <ruchamarathe15@gmail.com>
2017-07-25 04:53:37 -0700

    increase mtu size for veth

---
+++ veth_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for idx in 0 1 2 3 4 5 6 7 8; do
+    intf0="veth$(($idx*2))"
+    intf1="veth$(($idx*2+1))"
+    if ! ip link show $intf0 &> /dev/null; then
+        ip link add name $intf0 type veth peer name $intf1
+        ip link set dev $intf0 up
+        ip link set dev $intf1 up
+        # few test cases need larger mtu than in default
+        ifconfig $intf0 mtu 9500 up
+        ifconfig $intf1 mtu 9500 up
+    fi
+done
